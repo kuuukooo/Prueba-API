18ae6e9af3d2f974b11c156c5a0312aa1d856d72
José Ramírez <jwh743@gmail.com>
2025-05-12 10:45:33 -0300

    Server.js base subido

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import path from 'path';
-import session from 'express-session';
-import cookieParser from 'cookie-parser';
-import { connectDB } from './config/db.js';
-import productRoutes from './routes/product.route.js';
-import authRoutes from './routes/authRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -17,26 +14,17 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(
-  session({
-    secret: process.env.JWT_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { httpOnly: true, secure: false },
-  })
-);
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
+app.use("/api/products", productRoutes);
 
-app.use('/api/products', productRoutes);
-app.use('/api/auth', authRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   connectDB();
